4029a9270ade77aedde287057fa3fb88dbc76034
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-11-24 00:06:29 -0500

    DOM

---
+++ JavaScript/Leccion20-DOM/01-DOM.js
@@ -0,0 +1,10 @@
+// Document Object Model
+// En un documento html podemos encontrar diferentes etiquetas como html, head, dentro de head title...Dentro de una etiqueta body, h1, y dentro una etiqueta de texto...
+// Todos estos elementos componen lo que se conoce como Document Object Model
+// Básicamente cada uno de los elementos va a formar un DOM
+// El DOM es un estándar y cada uno de los elementos es un objeto cuando se trabaja desde JS
+// El objeto principal que va a acceder a nuestro DOM es el document. Así que el objeto document es el que vamos a utilizar para manipular nuestro documento html.
+// A través del objeto document, vamos a poder interactuar con cada uno de los elementos como lo son html, head, title, body, etc...
+// Por lo tanto con JS podemos trabajar y modificar cada uno de los elementos.
+// En JS cada elemento es un objeto, por lo tanto cada objeto tiene atributos, propiedades y métodos. Así que podremos acceder a los atributos y métodos de cada uno de los elementos.
+
